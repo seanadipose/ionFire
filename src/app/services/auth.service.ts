b6a0069a70c692a05332df337a88d2f457307ae3
sean <hamiltonsean6@gmail.com>
2019-01-20 15:41:54 -0800

    added methods for web google auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,6 +6,9 @@ import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { User } from '../models/user';
 import { IUser } from '../interfaces';
+import { Storage } from '@ionic/storage';
+import { Platform, LoadingController } from '@ionic/angular';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -33,13 +36,18 @@ export class AuthService {
     private afAuth: AngularFireAuth,
     private db: DbService,
     private router: Router,
+    private storage: Storage,
+    private platform: Platform,
+    private loadingController: LoadingController,
   ) {
     /* see if user exists by piping to a switch map that returns
     the user profile or returns a null observable if it's not created
     that */
-    // this.user$ = this.afAuth.authState.pipe(
-      // switchMap(user => (user ? this.db.doc$(`users/${user.uid}`) : of(null)))
-    // );
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => (user ? this.db.doc$(`users/${user.uid}`) : of(null)))
+    );
+
+    this.handleRedirect();
   }
 
   // anonymouse login method to log user in and create new one.
@@ -54,6 +62,50 @@ export class AuthService {
     await this.afAuth.auth.signOut();
     this.user$ = of(null);
     const test = await this.router.navigate(['/']);
+  }
+
+  // Google auth
+
+  setRedirect(val) {
+    this.storage.set('authRedirect', val);
+  }
+
+  async isRedirect() {
+    return await this.storage.get('authRedirect');
+  }
+
+  async googleLogin() {
+    try {
+      let user;
+      if (this.platform.is('cordova')) user = await this.nativeGoogleLogin();
+      else {
+        await this.setRedirect(true);
+        const provider = new auth.GoogleAuthProvider();
+        user = await this.afAuth.auth.signInWithRedirect(provider);
+      }
+      return await this.updateUserData(user);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Handle loginwith redirecgt for web google auth
+  private async handleRedirect() {
+    if (await this.isRedirect() !== true) {
+      return null;
+    }
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    const result = await this.afAuth.auth.getRedirectResult();
+
+    if (result.user) {
+      await this.updateUserData(result.user);
+    }
+
+    await loading.dismiss();
+    return result;
+
   }
 
 }
