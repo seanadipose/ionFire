1f5c024eb6412511ad83ab047e33050de7bf4cc9
sean <hamiltonsean6@gmail.com>
2019-01-20 13:55:03 -0800

    added route guard to validate completing the tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TutorialGuard } from './guards/tutorial.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  // home page is guarded by the tutorial guardd
+  { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [TutorialGuard] },
   { path: 'todo', loadChildren: './todo/todo.module#TodoPageModule' },
   { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialPageModule' },
 ];
--- src/app/guards/tutorial.guard.ts
+++ src/app/guards/tutorial.guard.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+
+/*
+Router guard to check local storage and validate that the user
+has completed the tutorial slider.
+It checks the ionic storage value of tutorial complete.
+Tutorial complete is set in the tutorial.page component
+*/
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialGuard implements CanActivate {
-  canActivate(
+  constructor(private storage: Storage, private router: Router) {}
+  async canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    const isComplete = await this.storage.get('tutorialComplete');
+    if (!isComplete) {
+      this.router.navigateByUrl('/tutorial');
+    }
+    return isComplete;
   }
 }
