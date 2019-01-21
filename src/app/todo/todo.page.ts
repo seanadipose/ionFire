378b5be4de7b85a5902f3bac5ba21be122995301
sean <hamiltonsean6@gmail.com>
2019-01-20 21:07:38 -0800

    added observable todos list

--- src/app/todo/todo.page.ts
+++ src/app/todo/todo.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { switchMap, map, shareReplay } from 'rxjs/operators';
+import { DbService } from '../services/db.service';
+import { AuthService } from '../services/auth.service';
+
+import { ModalController } from '@ionic/angular';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +13,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.page.scss'],
 })
 export class TodoPage implements OnInit {
+  todos;
+  filtered;
 
-  constructor() { }
+  filter = new BehaviorSubject(null);
+
+
+  constructor(
+    public db: DbService,
+    public modal: ModalController,
+    public auth: AuthService
+  ) { }
 
   ngOnInit() {
+    this.todos = this.auth.user$.pipe(
+      switchMap(user =>
+        this.db.collection$('todos', ref =>
+          ref
+            .where('uid', '==', user['uid'])
+            .orderBy('createdAt', 'desc')
+            .limit(25)
+        )
+      ),
+      shareReplay(1)
+    );
+  }
+
+  trackById(idx, todo) {
+    return todo.id;
   }
 
 }
